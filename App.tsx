b18a1db46313d1de5a291e149903057c1f0736dd
Aryan Balpande <aryan.balpande@somaiya.edu>
2024-01-15 12:29:30 +0530

    Updated to +-2

--- App.tsx
+++ App.tsx
@@ -6,11 +6,11 @@ const App = () => {
   const [count, setCount] = useState(0);
 
   const increment = () => {
-    setCount(count + 1);
+    setCount(count + 2);
   };
 
   const decrement = () => {
-    setCount(count - 1);
+    setCount(count - 2);
   };
 
   return (
